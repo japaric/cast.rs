ca5aaef2979b7badf02e3ef428edd511f858539a
est31 <MTest31@outlook.com>
2017-05-06 15:27:27 +0200

    Improve travis code

--- ci/script.sh
+++ ci/script.sh
@@ -4,8 +4,8 @@ main() {
     cross test --target $TARGET
     cross test --target $TARGET --release
 
-    [ -z "$RUST_NIGHTLY" ] || cross test --feature x128 --target $TARGET
-    [ -z "$RUST_NIGHTLY" ] || cross test --feature x128 --target $TARGET --release
+    [ "$TRAVIS_RUST_VERSION" -eq "nightly" ] && cross test --feature x128 --target $TARGET
+    [ "$TRAVIS_RUST_VERSION" -eq "nightly" ] && cross test --feature x128 --target $TARGET --release
 
     cross test --no-default-features --target $TARGET
     cross test --no-default-features --target $TARGET --release

6544a7b3fd31663e5593c34dee2912f9c53628b8
est31 <MTest31@outlook.com>
2017-05-06 05:01:56 +0200

    Also test with x128 feature on

--- ci/script.sh
+++ ci/script.sh
@@ -4,6 +4,9 @@ main() {
     cross test --target $TARGET
     cross test --target $TARGET --release
 
+    [ -z "$RUST_NIGHTLY" ] || cross test --feature x128 --target $TARGET
+    [ -z "$RUST_NIGHTLY" ] || cross test --feature x128 --target $TARGET --release
+
     cross test --no-default-features --target $TARGET
     cross test --no-default-features --target $TARGET --release
 }

4c23cbe8d8de2a6ab198c7d7e9e41abcb9d0032e
Jorge Aparicio <jorge@japaric.io>
2019-11-16 18:27:23 -0500

    update ci

--- ci/script.sh
+++ ci/script.sh
@@ -1,14 +1,43 @@
-set -ex
+set -euxo pipefail
 
 main() {
-    cross test --target $TARGET
-    cross test --target $TARGET --release
+    cargo check --target $TARGET --no-default-features
 
-    [ "$TRAVIS_RUST_VERSION" -eq "nightly" ] && cross test --feature x128 --target $TARGET
-    [ "$TRAVIS_RUST_VERSION" -eq "nightly" ] && cross test --feature x128 --target $TARGET --release
+    cargo test --target $TARGET
+    cargo test --target $TARGET --release
 
-    cross test --no-default-features --target $TARGET
-    cross test --no-default-features --target $TARGET --release
+    # not MSRV
+    if [ $TRAVIS_RUST_VERSION != 1.13.0 ]; then
+        cargo test --features x128 --target $TARGET
+        cargo test --features x128 --target $TARGET --release
+    fi
 }
+
+# fake Travis variables to be able to run this on a local machine
+if [ -z ${TRAVIS_BRANCH-} ]; then
+    TRAVIS_BRANCH=staging
+fi
+
+if [ -z ${TRAVIS_PULL_REQUEST-} ]; then
+    TRAVIS_PULL_REQUEST=false
+fi
+
+if [ -z ${TRAVIS_RUST_VERSION-} ]; then
+    case $(rustc -V) in
+        *nightly*)
+            TRAVIS_RUST_VERSION=nightly
+            ;;
+        *beta*)
+            TRAVIS_RUST_VERSION=beta
+            ;;
+        *)
+            TRAVIS_RUST_VERSION=stable
+            ;;
+    esac
+fi
+
+if [ -z ${TARGET-} ]; then
+    TARGET=$(rustc -Vv | grep host | cut -d ' ' -f2)
+fi
 
 main
